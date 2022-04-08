b7a64c87a066bcf5a137053701bcbf2f62a30c6c
Icankkkk <muhamadfaisal280201@gmail.com>
2022-04-08 14:11:45 +0700

    add pertemuan-01

---
+++ Pertemuan-01/README.md
@@ -0,0 +1,25 @@
+### Main Of Topic
+
+Pada pertemuan kali ini kita sudah mempelajari sedikit tentang fudamental C++ yaitu:
+- [Variabel & Literals][var-lit]
+- [Data Types][data-type]
+- [I/O Base][io-base]
+
+<hr>
+
+### Link Referensi
+
+Website-website rekomended untuk belajar pemograman C++:
+- [belajar cpp][bcpp]
+- [w3schools][w3]
+- [duniailkom][di]
+
+
+
+[var-lit]: <https://www.programiz.com/cpp-programming/variables-literals>
+[data-type]: <https://www.programiz.com/cpp-programming/data-types>
+[io-base]: <https://www.programiz.com/cpp-programming/input-output>
+
+[bcpp]: <https://www.belajarcpp.com/>
+[w3]: <https://www.w3schools.com/CPP/default.asp>
+[di]: <https://www.duniailkom.com/tutorial-belajar-bahasa-pemrograman-c-plus-plus-untuk-pemula/>
---
+++ Pertemuan-01/src/Main.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /*
+    int angka2; // deklarasi
+    angka2 = 50; // = (penugasan)
+
+    int angka1 = 30; // inisialisasi awal
+    //cout << "angka1: " << angka1 << '\n';
+
+    angka1 = 60;    // gak eror
+    cout << "angka1: " << angka1 << endl;
+
+    // variabel konstanta
+    // const tipeData namaVar = value;
+    const float phi = 3.14; 
+
+    int angka_satu;
+    int _angka_satu;
+
+    int var = 20;
+    float berat = 80.7;
+    string nama = "hanapi";
+    char karakter = 'a';
+    bool benar = false; // true & false
+
+    cout << var << '\n';
+    cout << berat << '\n';
+    cout << nama << "\n";
+    cout << karakter << '\n';
+    cout << benar << '\n';
+
+    // kondisi
+    if (!benar) {
+        cout << "hello\n"; // statement
+    } else {
+        cout << "world\n";
+    }
+
+    string namaLengkap;
+    char alamat[30];
+
+    cout << "Masukan nama lengkap :";
+    getline(cin, namaLengkap); // cin >> namaLengkap;
+    cout << "Masukan alamat : ";
+    cin.getline(alamat, 30);
+
+    cout << "Nama anda : " << namaLengkap << '\n';
+     cout << "Alamat anda : " << alamat << '\n';
+
+    int num1, num2, num3;
+
+    cin >> num1 >> num2 >> num3;
+    // getline()
+    cout << "==============================\n";
+    cout << num1 << '\n' << num2 << '\n' << num3 << '\n';
+    */
+
+   /*
+    string nama;
+    string namaMahasiswa[5] = {"icang", "aden", "nur", "firman", "jafar"};
+
+    int nilai[3];   // deklarasi awal sebuah array berdimensi 1 dengan indeks 3
+    int i;
+
+    // input data
+    for ( i = 0; i < 3; i++) {
+        cout << "Data ke-" << i+1 << " : ";
+        cin >> nilai[i];
+    }
+
+    cout << "\noutput:\n\n";
+    // output data
+    for ( i = 0; i < 3; i++) {
+        cout << "Data ke-" << i+1 << " : ";
+        cout << nilai[i] << '\n';
+    } */
+
+     int var = 2;
+
+    cout<<var<<endl;
+    cout<<&var<<endl;
+    cout<<*&var<<endl;
+
+    return 0;
+}
+
+
+/*
+escapen sequence:
+https://www.programiz.com/cpp-programming/variables-literals
+*/
